4834091b2de43a4573326074fa7b97f9dbd89057
Tompizza <108546980+Tompizza@users.noreply.github.com>
2023-02-05 03:16:30 +0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+ 
+Config = {}
+
+Config.objects = {
+ 
+ 
+	BedSitAnimation = {dict='switch@trevor@annoys_sunbathers', anim='trev_annoys_sunbathers_loop_guy'},
+	locations = {
+		{objName="v_med_bed2", verticalOffsetX=0.0, verticalOffsetY=0.0, verticalOffsetZ=0.0, direction=0.0, bed=true}, 
+	}
+}
+ 
+ 
+
+Config.job = 'ambulance'  
+
+ 
+ 
+
+ 
